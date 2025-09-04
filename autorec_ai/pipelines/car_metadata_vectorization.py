3bae37521e0a063ddf930c4bfe9d1b3bf6bd7c20
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2025-09-04 22:38:20 +0100

    feat: track data with DVC

---
+++ autorec_ai/pipelines/car_metadata_vectorization.py
@@ -0,0 +1,5 @@
+from autorec_ai.preprocessing.metadata import Vectorizer
+
+if __name__ == '__main__':
+    car_metadata_vectorizer = Vectorizer(qdrant_grpc_host='0.0.0.0', qdrant_grpc_port=6334)
+    car_metadata_vectorizer.vectorize_and_upload()
