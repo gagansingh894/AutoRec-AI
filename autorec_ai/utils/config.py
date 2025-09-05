e1edbd560fd9653469efc980ab75bcd751158003
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2025-09-05 01:03:02 +0100

    feat: add model specif config

--- autorec_ai/utils/config.py
+++ autorec_ai/utils/config.py
@@ -3,5 +3,9 @@ METADATA_PATH = "data/car_metadata"
 GROUPED_BY_CAR_MAKE_MODEL_YEAR_DATA_PATH = "data/raw_grouped_by_car_make_model_year"
 PROCESSED_DATA_PATH = "data/processed/augmented_grouped_by_car_make_model_year"
 PROCESSED_AUGMENTATION_ANALYSIS_REPORT_DATA_PATH = "data/processed"
+
 QDRANT_COLLECTION_NAME = 'car_metadata'
 QDRANT_BATCH_SIZE = 128
+
+NUMBER_OF_CLASSES = 2017
+CUSTOM_MOBILENET_PATH = "models"
